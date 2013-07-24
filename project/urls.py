cc47117334a83527f6ef4432c7d6a75780282b1c
Gabriel Samfira <samfiragabriel@gmail.com>
2013-07-24 11:57:05 +0300

    updated urls.py

--- project/urls.py
+++ project/urls.py
@@ -11,8 +11,8 @@ admin.autodiscover()
 
 urlpatterns = patterns(
     '',
-    url(r'^admin/doc/', include('django.contrib.admindocs.urls')),
-    url(r'^admin/', include(admin.site.urls)),
+    url(r'^admin/doc[/]?', include('django.contrib.admindocs.urls')),
+    url(r'^admin[/]?', include(admin.site.urls)),
     url(r'^api', include('project.api.urls')),
     url(r'^ip[/]?$', backend_views.getIP),
     url(r'^stats[/]?$', stat_views.statistics),
